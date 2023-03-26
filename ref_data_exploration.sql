3a21afa050da3dda60a77348c835084707bfd52f
= <tc43722@gmail.com>
2023-03-26 12:23:14 +0100

    test2

--- ref_data_exploration.sql
+++ ref_data_exploration.sql
@@ -21,7 +21,7 @@ CREATE TABLE raw_data(
 
 -- import data (run from psql):
 -- \copy raw_data FROM 'C:\Users\...filepath...\raw_data.csv' WITH CSV HEADER;
--- comment test again
+-- This is just a test
 
 -- add primary key:
 ALTER TABLE raw_data ADD COLUMN id SERIAL PRIMARY KEY;
